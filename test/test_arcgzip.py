f7063a7bb810022b63d0a71a470d34a9f3b2ed31
Fujimoto <fujimoto@writingarchives.sakura.ne.jp>
2014-10-07 02:12:41 +0900

    Make 'write' test to create an actual gzip file.

--- test/test_arcgzip.py
+++ test/test_arcgzip.py
@@ -1,6 +1,8 @@
 import unittest
 import os
 import io
+import tempfile
+import shutil
 from arcgzip import GzipFile, GzipInfo
 
 DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
@@ -39,39 +41,39 @@ class TestWriteGzip(unittest.TestCase):
     ## TEST SETTINGS
     TEST_FILE = os.path.join(DATA_DIR, "test.txt")
 
+    def setUp(self):
+        self.tmpdir = tempfile.mkdtemp()
+
+    def tearDown(self):
+        shutil.rmtree(self.tmpdir)
+
     ## Test cases
     def test_write_attributes(self):
-        rawio = io.BytesIO()
+        filepath = os.path.join(self.tmpdir, "test.gz")
 
-        gzip_writable = GzipFile(rawio, mode="w")
-        gzip_writable.addfile(self.TEST_FILE)
+        with GzipFile.open(filepath, mode="w") as gzip:
+            gzip.addfile(self.TEST_FILE)
 
-        rawio.seek(0)
+        with GzipFile.open(filepath, mode="r") as gzip:
+            info = gzip.getinfo("test.txt")
 
-        gzip_readable = GzipFile(rawio, mode="r")
-
-        info =  gzip_readable.gzipinfos[0]
-    
-        self.assertEqual(info.CM, 8)
-        self.assertEqual(info.FLG, 0b00001000)
-        self.assertEqual(info.MTIME, int(os.path.getmtime(self.TEST_FILE)))
-        self.assertEqual(info.XFL, 0)
-        self.assertEqual(info.FNAME, os.path.basename(self.TEST_FILE))
+            self.assertEqual(info.CM, 8)
+            self.assertEqual(info.FLG, 0b00001000)
+            self.assertEqual(info.MTIME, int(os.path.getmtime(self.TEST_FILE)))
+            self.assertEqual(info.XFL, 0)
+            self.assertEqual(info.FNAME, os.path.basename(self.TEST_FILE))
 
     def test_write_contents(self):
-        rawio = io.BytesIO()
+        filepath = os.path.join(self.tmpdir, "test.gz")
 
-        gzip_writable = GzipFile(rawio, mode="w")
-        gzip_writable.addfile(self.TEST_FILE)
+        with GzipFile.open(filepath, mode="w") as gzip:
+            gzip.addfile(self.TEST_FILE)
 
-        rawio.seek(0)
+        with GzipFile.open(filepath, mode="r") as gzip, \
+             open(self.TEST_FILE, mode="rb") as orig:
 
-        gzip_readable = GzipFile(rawio, mode="r")
-
-        extr = gzip_readable.extract(os.path.basename(self.TEST_FILE))
-        orig = open(self.TEST_FILE, "rb")
-
-        self.assertEqual(extr.read(), orig.read())
+            fp = gzip.extract("test.txt")
+            self.assertEqual(fp.read(), orig.read())
         
 if __name__ == "__main__":
     unittest.main()
