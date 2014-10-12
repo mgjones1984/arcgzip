450326d83f02dac82384eedcb83c6b58fea32fb3
Fujimoto <fujimoto@writingarchives.sakura.ne.jp>
2014-10-12 23:57:11 +0900

    Add test case to confirm that files with trailing bytes are still readable.

--- test/test_arcgzip.py
+++ test/test_arcgzip.py
@@ -56,11 +56,17 @@ class TestReadGzip(unittest.TestCase):
 
 class TestReadInvalidFiles(unittest.TestCase):
     EMPTY_FILE = os.path.join(DATA_DIR, "emptyfile.gz")
+    EXBYTES_FILE =  os.path.join(DATA_DIR, "extrabytes.gz")
 
     def test_read_emptyfile(self):
         with self.assertRaises(IOError):
             with GzipFile.open(self.EMPTY_FILE) as gzip:
                 pass
+
+    def test_extra_bytes(self):
+        """ Test case for files with trailing garbage bytes """
+        with GzipFile.open(self.EXBYTES_FILE) as gzip:
+            self.assertEqual(len(gzip.gzipinfos), 1)
 
 class TestWriteGzip(unittest.TestCase):
     ## TEST SETTINGS
