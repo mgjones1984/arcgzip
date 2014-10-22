484b6e93ec3ddf54d6467eed68f542647ba34026
Fujimoto <fujimoto@writingarchives.sakura.ne.jp>
2014-10-22 23:52:16 +0900

    Split tests into several modules (for clarity)

---
+++ test/test_read_gzip.py
@@ -0,0 +1,57 @@
+import unittest
+import os
+import tempfile
+import shutil
+from arcgzip import GzipFile, GzipInfo, GzipError
+
+DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
+
+class TestReadGzip(unittest.TestCase):
+    ## TEST SETTINGS
+    TEST_FILE = os.path.join(DATA_DIR, "test.txt.gz")
+    FILE_ATTR = {
+        "CM": 8,             # DEFLATE
+        "FLG": 0b00001000,   # FNAME flag
+        "MTIME": 1412132400, # '2014/10/1 12:00:00'
+        "XFL": 0,
+        "OS": 3,             # UNIX
+        "FNAME": "test.txt"
+    }
+    FILE_CONTENTS = b"asparagus\n"
+
+    ## Test cases
+    def test_load_file(self):
+        with GzipFile.open(self.TEST_FILE) as gzip:
+            self.assertEqual(len(gzip.gzipinfos), 1)
+
+    def test_check_attributes(self):
+        with GzipFile.open(self.TEST_FILE) as gzip:
+            info = gzip.gzipinfos[0]
+
+            for key in self.FILE_ATTR:
+                self.assertEqual(getattr(info, key), self.FILE_ATTR[key])
+
+    def test_read_contents(self):
+        with GzipFile.open(self.TEST_FILE) as gzip:
+            fp = gzip.extract(self.FILE_ATTR["FNAME"])
+            self.assertEqual(fp.read(), self.FILE_CONTENTS)
+
+    def test_extract_file(self):
+        tmpdir = None
+        try:
+            tmpdir = tempfile.mkdtemp()
+            os.chdir(tmpdir)
+
+            with GzipFile.open(self.TEST_FILE) as gzip:
+                filename = self.FILE_ATTR["FNAME"]
+                gzip.extractfile(filename)
+
+                stat = os.stat(filename)
+                self.assertEqual(stat.st_mtime, self.FILE_ATTR["MTIME"])
+                self.assertEqual(stat.st_size, len(self.FILE_CONTENTS))
+        finally:
+            if tmpdir:
+                shutil.rmtree(tmpdir)
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ test/test_read_invalid.py
@@ -0,0 +1,29 @@
+import unittest
+import os
+from arcgzip import GzipFile, GzipInfo, GzipError
+
+DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
+
+class TestReadInvalidFiles(unittest.TestCase):
+    EMPTY_FILE = os.path.join(DATA_DIR, "emptyfile.gz")
+    EXBYTES_FILE =  os.path.join(DATA_DIR, "extrabytes.gz")
+    EXMAGIC_FILE =  os.path.join(DATA_DIR, "extramagic.gz")
+
+    def test_read_emptyfile(self):
+        with self.assertRaises(IOError):
+            with GzipFile.open(self.EMPTY_FILE) as gzip:
+                pass
+
+    def test_extra_bytes(self):
+        """ Test case for files with trailing garbage bytes """
+        with GzipFile.open(self.EXBYTES_FILE) as gzip:
+            self.assertEqual(len(gzip.gzipinfos), 1)
+
+    def test_extra_magic(self):
+        """ Special case in which trailing bytes are magic numbers"""
+        with self.assertRaises(GzipError):
+            with GzipFile.open(self.EXMAGIC_FILE) as gzip:
+                pass
+
+if __name__ == "__main__":
+    unittest.main()
