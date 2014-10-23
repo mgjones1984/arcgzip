75c9a61fbd5eeb63d956889d29a74d6833112764
Fujimoto <fujimoto@writingarchives.sakura.ne.jp>
2014-10-23 23:40:31 +0900

    Hold the target file name as a variable

--- test/test_write_gzip.py
+++ test/test_write_gzip.py
@@ -8,7 +8,8 @@ DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
 
 class TestWriteGzip(unittest.TestCase):
     ## TEST SETTINGS
-    TEST_FILE = os.path.join(DATA_DIR, "textfile")
+    FILE_NAME = 'textfile'
+    TEST_FILE = os.path.join(DATA_DIR, FILE_NAME)
 
     def setUp(self):
         self.tmpdir = tempfile.mkdtemp()
@@ -24,7 +25,7 @@ class TestWriteGzip(unittest.TestCase):
             gzip.addfile(self.TEST_FILE)
 
         with GzipFile.open(filepath, mode="r") as gzip:
-            info = gzip.getinfo("textfile")
+            info = gzip.getinfo(self.FILE_NAME)
 
             self.assertEqual(info.CM, 8)
             self.assertEqual(info.FLG, 0b00001000)
@@ -41,7 +42,7 @@ class TestWriteGzip(unittest.TestCase):
         with GzipFile.open(filepath, mode="r") as gzip, \
              open(self.TEST_FILE, mode="rb") as orig:
 
-            fp = gzip.extract("textfile")
+            fp = gzip.extract(self.FILE_NAME)
             self.assertEqual(fp.read(), orig.read())
 
     def test_best_compression(self):
@@ -51,7 +52,7 @@ class TestWriteGzip(unittest.TestCase):
             gzip.addfile(self.TEST_FILE, compresslevel=9)
 
         with GzipFile.open(filepath, mode="r") as gzip:
-            info = gzip.getinfo("textfile")
+            info = gzip.getinfo(self.FILE_NAME)
             self.assertEqual(info.XFL, 2)
 
     def test_best_speed(self):
@@ -61,7 +62,7 @@ class TestWriteGzip(unittest.TestCase):
             gzip.addfile(self.TEST_FILE, compresslevel=1)
 
         with GzipFile.open(filepath, mode="r") as gzip:
-            info = gzip.getinfo("textfile")
+            info = gzip.getinfo(self.FILE_NAME)
             self.assertEqual(info.XFL, 4)
 
     def test_file_comment(self):
@@ -72,7 +73,7 @@ class TestWriteGzip(unittest.TestCase):
             gzip.addfile(self.TEST_FILE, comment=comment)
 
         with GzipFile.open(filepath, mode="r") as gzip:
-            info = gzip.getinfo("textfile")
+            info = gzip.getinfo(self.FILE_NAME)
             self.assertEqual(comment, info.FCOMMENT)
 
     def test_crc16(self):
@@ -82,7 +83,7 @@ class TestWriteGzip(unittest.TestCase):
             gzip.addfile(self.TEST_FILE, crc16=True)
 
         with GzipFile.open(filepath, mode="r") as gzip:
-            info = gzip.getinfo("textfile")
+            info = gzip.getinfo(self.FILE_NAME)
             self.assertIsNotNone(info.CRC16)
 
 if __name__ == "__main__":
