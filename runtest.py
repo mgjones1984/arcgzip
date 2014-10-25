032762839cb150b7cff099884541ee8a87f6f931
Fujimoto <fujimoto@writingarchives.sakura.ne.jp>
2014-10-25 21:54:25 +0900

    Improve test runner script.

--- runtest.py
+++ runtest.py
@@ -2,10 +2,23 @@
 
 import logging
 import unittest
+import getopt
+import sys
 
-if __name__ == "__main__":
+def main():
+    opts, args = getopt.getopt(sys.argv[1:], 'v')
+    verbosity = 1
+    testdir = 'test/'
+
+    for key, val in opts:
+        if key == '-v':
+            verbosity += 1
+
     logging.disable(logging.WARNING)
 
-    suite = unittest.defaultTestLoader.discover('./test')
-    runner = unittest.TextTestRunner()
+    suite = unittest.defaultTestLoader.discover(testdir)
+    runner = unittest.TextTestRunner(verbosity=verbosity)
     runner.run(suite)
+
+if __name__ == "__main__":
+    main()
